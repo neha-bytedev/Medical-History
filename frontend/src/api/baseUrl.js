fe68120f1f53c4cf77285d6231394a98c70787c1
neha-bytedev <neha.saman567@gmail.com>
2026-01-13 14:42:04 +0530

    done

--- frontend/src/api/baseUrl.js
+++ frontend/src/api/baseUrl.js
@@ -1,2 +1,2 @@
 //export const baseUrl = "https://photo-share-backend-iota.vercel.app"
-export const baseUrl= "http://localhost:9500"
+export const baseUrl= "https://medical-history-backend.vercel.app"
